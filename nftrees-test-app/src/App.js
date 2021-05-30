c59eb19c4829822c78a168644dbf5e1f3dd68890
Brendan Morrison <b.k.morrison2@gmail.com>
2021-05-30 11:55:18 -0700

    add loader

--- nftrees-test-app/src/App.js
+++ nftrees-test-app/src/App.js
@@ -1,10 +1,27 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Landing from './components/Landing';
+import BounceLoader from 'react-spinners/BounceLoader'
 
 function App() {
+  const [Loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    setLoading(false);
+  }, [])
+
+  function loading () {
+    return(<BounceLoader size = {100}/>)
+  }
+
+  function notLoading () {
+    return(<Landing/>)
+  }
+
   return (
     <div className="App">
-      <Landing/>
+      {Loading ? loading() : notLoading()}
     </div>
   );
 }
